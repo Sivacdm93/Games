45cdbbef444126789c399060d2b1a479581f883a
Sivacdm93 <sivachidambaram2393@gmail.com>
2025-10-07 17:14:04 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Admin secret used when you open the site with ?admin=1
+// Example: https://<user>.github.io/Games/?admin=1
+window.ADMIN_CODE = "VIBE-SECRET-1234";
+
+// Your Firebase web app config
+window.firebaseConfig = {
+  apiKey: "AIzaSyAmmehhmU_ZKZ-rdnQ1e6PULO2bd1fYisg",
+  authDomain: "team-vibe-1c3ea.firebaseapp.com",
+  projectId: "team-vibe-1c3ea",
+  storageBucket: "team-vibe-1c3ea.firebasestorage.app",
+  messagingSenderId: "970729517323",
+  appId: "1:970729517323:web:2f0034d39ca6c22702f20e"
+};
